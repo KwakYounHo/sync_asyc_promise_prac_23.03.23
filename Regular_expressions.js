223a09803d8c7537660c563193a8d4c1a94d222b
KwakYounHo <kwakyh951@gmail.com>
2023-03-23 16:26:38 +0900

    \s 테스트와 A-z테스트

--- Regular_expressions.js
+++ Regular_expressions.js
@@ -24,5 +24,14 @@
 // ^  -> 문장 시작
 // $  -> 문장 끝
 
-const E = /^[\w]{4,12}$/
-console.log(E.test('abc_A123'))
+// const E = /^[\w]{4,12}$/
+// console.log(E.test('abc_A123'))
+
+// const F = /\s/
+// console.log(F.test('abc_12 3d'))
+
+// const G = /^[\w]{4,12}$/
+// console.log(G.test('abc_123[]'))
+
+// const H = /^[A-z0-9]{4,12}$/   // ----> A-z 사이에는 (-),(_),([]),(^)문자가 포함되어 있음
+// console.log(H.test('abc_DEF123[]^'))
